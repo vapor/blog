05f16cd03ed94bad0a8f3050054db8550beb9c9c
Tim <0xtimc@gmail.com>
2022-02-22 12:31:47 +0000

    Refactor components out

---
+++ Sources/Blog/Components/ItemList.swift
@@ -0,0 +1,18 @@
+import Plot
+import Publish
+
+struct ItemList<Site: Website>: Component {
+    var items: [Item<Site>]
+    var site: Site
+
+    var body: Component {
+        List(items) { item in
+            Article {
+                H1(Link(item.title, url: item.path.absoluteString))
+                ItemTagList(item: item, site: site)
+                Paragraph(item.description)
+            }
+        }
+        .class("item-list")
+    }
+}
