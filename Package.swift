30abc7056632f04270482e0185e1de65e36a5205
Tim <0xtimc@gmail.com>
2022-12-30 22:24:35 +0000

    Update Swift tools version

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.7
 
 import PackageDescription
 
@@ -11,12 +11,14 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(name: "Publish", url: "https://github.com/johnsundell/publish.git", from: "0.8.0")
+        .package(url: "https://github.com/johnsundell/publish.git", from: "0.8.0")
     ],
     targets: [
         .target(
             name: "Blog",
-            dependencies: ["Publish"]
+            dependencies: [
+                .product(name: "Publish", package: "publish")
+            ]
         )
     ]
 )
