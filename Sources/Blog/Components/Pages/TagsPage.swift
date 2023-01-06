8a6b9f2457e706349a50cddba593fd59fddcdeec
Tim <0xtimc@gmail.com>
2023-01-06 09:43:10 +0000

    Remove old code

--- Sources/Blog/Components/Pages/TagsPage.swift
+++ Sources/Blog/Components/Pages/TagsPage.swift
@@ -56,53 +56,5 @@ struct TagsPage: Component {
             }
         }.class("container blog-container")
         SiteFooter(currentSite: .blog)
-        //        H1("Blog tags")
-        //        List(context.allTagLinks) { tagLink in
-        //            var name = tagLink.name
-        //            if tagLink.name == selectedTag?.string ||
-        //                (tagLink.path == context.site.tagListPath && selectedTag == nil) {
-        //                name = "[ " + name + " ]"
-        //            }
-        //            return ListItem {
-        //                Link(name, url: tagLink.path.absoluteString)
-        //            }
-        //            .class("tag")
-        //        }
-        //        .class("all-tags")
-        //        .class("browse-all")
-        //
-        //        ItemList(
-        //            items: items,
-        //            site: context.site
-        //        )
-        //
-        //        if let selectedTag = selectedTag {
-        //            PaginationList(
-        //                numberOfPages: context.paginatedItems(for: selectedTag).count,
-        //                activePage: pageNumber,
-        //                pageURL: { context.site.paginatedPath(for: selectedTag, pageIndex: $0 - 1).absoluteString }
-        //            )
-        //        } else {
-        //            PaginationList(
-        //                numberOfPages: context.paginatedItems.count,
-        //                activePage: pageNumber,
-        //                pageURL: { context.site.paginatedTagListPath(pageIndex: $0 - 1).absoluteString }
-        //            )
-        //        }
-    }
-}
-
-extension TagsPage {
-    struct TagLink {
-        var name: String
-        var path: Path
-    }
-}
-
-extension PublishingContext where Site == Blog {
-    var allTagLinks: [TagsPage.TagLink] {
-        var tagLinks = [TagsPage.TagLink(name: "View all", path: site.tagListPath)]
-        tagLinks.append(contentsOf: allTags.sorted().map { .init(name: $0.string, path: site.path(for: $0)) })
-        return tagLinks
     }
 }
