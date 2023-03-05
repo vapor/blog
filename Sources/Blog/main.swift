66be86980021727e006f36257d74dc72fd6d5fb1
Gwynne Raskind <gwynne@vapor.codes>
2023-03-05 15:02:59 -0600

    Add support for specifying additional contributing authors in blog post metadata. Relies on the identically named branch in vapor/design.

--- Sources/Blog/main.swift
+++ Sources/Blog/main.swift
@@ -10,10 +10,32 @@ struct Blog: Website {
         case posts
     }
 
+    static var authorImagePlaceholder: String { "https://design.vapor.codes/images/author-image-placeholder.png" }
+
     struct ItemMetadata: WebsiteItemMetadata {
         // Add any site-specific metadata that you want to use here.
-        var author: String
+        var author: String?
+        var authors: String?
         var authorImageURL: String?
+        var authorImageURLs: String?
+        
+        var allAuthors: [String] {
+            let authors = ((self.author ?? "").split(separator: ";") + (self.authors ?? "").split(separator: ";"))
+                .map({ $0.trimmingCharacters(in: .whitespaces) })
+            
+            return authors.isEmpty ? ["Unknown"] : authors
+        }
+        
+        var allAuthorImageURLs: [String] {
+            var imageURLs = ((self.authorImageURL ?? "").split(separator: ";") + (self.authorImageURLs ?? "").split(separator: ";"))
+                .map({ $0.trimmingCharacters(in: .whitespaces) })
+            let numAuthors = self.allAuthors.count
+
+            if imageURLs.count < numAuthors {
+                imageURLs.append(contentsOf: Array(repeating: Blog.authorImagePlaceholder, count: numAuthors - imageURLs.count))
+            }
+            return imageURLs
+        }
     }
 
     // Update these properties to configure your website:
